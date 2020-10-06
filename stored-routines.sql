40939835c96bf5004ccd7d4d569be4c5c0522101
madhuvayya <madhu.vayya@gmail.com>
2020-10-06 22:51:52 +0530

    Added query to create a simple function to calculate area of cicle

--- stored-routines.sql
+++ stored-routines.sql
@@ -4,3 +4,25 @@ SELECT * FROM cars;
 
 -- calling the procedure
 CALL AllCars();
+
+-- Simple function
+
+-- this function computes the area
+-- of a circle; it takes a radius as
+-- a parameter
+
+DELIMITER $$
+DROP FUNCTION IF EXISTS CircleArea;
+CREATE FUNCTION CircleArea(r DOUBLE) 
+RETURNS DOUBLE
+DETERMINISTIC
+BEGIN
+    DECLARE area DOUBLE;
+
+    SET area = r * r * pi();
+    RETURN area;
+END $$
+DELIMITER ;
+
+-- calling the function CircleArea()
+SELECT CircleArea(5.5);
